0e38993986cde2dc81107781385e1edb2f5a3a24
Dimitris Adamou <t8140002@aueb.gr>
2020-11-13 15:09:54 +0200

    new commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello good World"); 
         System.out.println("this is my personal branch??");
+        System.out.println("So how does this thing works??");
     }
 }
