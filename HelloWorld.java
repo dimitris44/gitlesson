d89607099dbe39febb1ba2ad04c91aee1f3f7ad1
Dimitris Adamou <t8140002@aueb.gr>
2020-11-13 16:14:40 +0200

    my third branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello good World"); 
-        System.out.println("this is my personal branch??");
-        System.out.println("So how does this thing works??");
-        System.out.println("so all the changes become here??");
-        System.out.println("now at dimitris??");
+        System.out.println("this is my third branch??");
     }
 }
